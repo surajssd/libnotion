a7467da007c3456f2ac491d121bcb41ba47f5c87
Suraj Deshmukh <surajd.service@gmail.com>
2022-01-10 16:50:46 +0530

    blocks: Add new API and rest helper function
    Signed-off-by: Suraj Deshmukh <surajd.service@gmail.com>

---
+++ api/blocks/types.go
@@ -0,0 +1,83 @@
+package blocks
+
+import "github.com/surajssd/libnotion/api"
+
+// PageResponseList is used to parse the response when querying pages endpoint.
+type BlockResponseList struct {
+	Response api.Response `json:",inline"`
+	Results  []Block      `json:"results,omitempty"`
+}
+
+type Block struct {
+	api.CommonObject `json:",inline"`
+	HasChildren      bool      `json:"has_children,omitempty"`
+	Archived         bool      `json:"archived,omitempty"`
+	Type             *BT       `json:"type,omitempty"`
+	BulletedListItem *Property `json:"bulleted_list_item,omitempty"`
+	NumberedListItem *Property `json:"numbered_list_item,omitempty"`
+	Callout          *Property `json:"callout,omitempty"`
+	Paragraph        *Property `json:"paragraph,omitempty"`
+	Heading1         *Property `json:"heading_1,omitempty"`
+	Heading2         *Property `json:"heading_2,omitempty"`
+	Heading3         *Property `json:"heading_3,omitempty"`
+	Todo             *Property `json:"to_do,omitempty"`
+	Quote            *Property `json:"quote,omitempty"`
+
+	// If the item is Toggle then we should go into this to explore all the blocks.
+	Toggle *Property `json:"toggle,omitempty"`
+}
+
+type Property struct {
+	Text    []FullText `json:"text,omitempty"`
+	Icon    *Icon      `json:"icon,omitempty"`
+	Checked bool       `json:"checked,omitempty"`
+}
+
+type Icon struct {
+	Type  string `json:"type,omitempty"`
+	Emoji string `json:"emoji,omitempty"`
+}
+
+type FullText struct {
+	Type        string       `json:"type,omitempty"`
+	Text        *Text        `json:"type,omitempty"`
+	Annotations *Annotations `json:"annotations,omitempty"`
+	PlainText   string       `json:"plain_text,omitempty"`
+}
+
+type Text struct {
+	Content string `json:"content,omitempty"`
+	Link    *Link  `json:"link,omitempty"`
+}
+
+type Link struct {
+	URL string `json:"url,omitempty"`
+}
+
+type Annotations struct {
+	Bold          bool   `json:"bold,omitempty"`
+	Italic        bool   `json:"italic,omitempty"`
+	Strikethrough bool   `json:"strikethrough,omitempty"`
+	Underline     bool   `json:"underline,omitempty"`
+	Code          bool   `json:"code,omitempty"`
+	Color         string `json:"color,omitempty"`
+}
+
+// BlockType
+type BT string
+
+var (
+	BTBulletListItem = BT("bulleted_list_item")
+	NumberedListItem = BT("numbered_list_item")
+	BTCallout        = BT("callout")
+	BTParagraph      = BT("paragraph")
+	BTHeading1       = BT("heading_1")
+	BTHeading2       = BT("heading_2")
+	BTHeading3       = BT("heading_3")
+	BTTodo           = BT("to_do")
+	BTQuote          = BT("quote")
+	BTToggle         = BT("toggle")
+	BTUnsupported    = BT("unsupported")
+	BTTable          = BT("table")
+	BTDivider        = BT("divider")
+)
---
+++ pkg/rest/list-all-blocks.go
@@ -0,0 +1,87 @@
+package rest
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"path"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/surajssd/libnotion/api"
+	"github.com/surajssd/libnotion/api/blocks"
+)
+
+func (nc *NotionClient) ListBlocks(id string) ([]blocks.Block, error) {
+	hasMore := true
+	startCursor := ""
+	var ret []blocks.Block
+	client := &http.Client{}
+
+	u, err := url.Parse(APIURL)
+	if err != nil {
+		return nil, fmt.Errorf("parsing the APIURL: %w", err)
+	}
+
+	u.Path = path.Join(u.Path, SubPathBlocks, id, "children")
+	listURL := u.String()
+
+	for hasMore {
+		req, err := http.NewRequest("GET", listURL, nil)
+		if err != nil {
+			return nil, fmt.Errorf("building query: %w", err)
+		}
+
+		req.Header.Add("Notion-Version", NotionVersion)
+		req.Header.Add("Authorization", "Bearer "+nc.token)
+		req.Header.Add("Content-Type", "application/json")
+
+		if startCursor != "" {
+			q := req.URL.Query()
+			q.Add("start_cursor", startCursor)
+			q.Add("page_size", "100")
+			req.URL.RawQuery = q.Encode()
+		}
+
+		resp, err := client.Do(req)
+		if err != nil {
+			return nil, fmt.Errorf("listing block entries: %w", err)
+		}
+
+		defer resp.Body.Close()
+
+		data, respErr := io.ReadAll(resp.Body)
+
+		if resp.StatusCode != http.StatusOK {
+			failedResp := api.FailureResponse{}
+
+			if respErr != nil {
+				log.Debugf("reading the response: %v", err)
+			} else {
+				if err := json.Unmarshal(data, &failedResp); err != nil {
+					log.Debugf("unmarshalling failure response: %v", err)
+				}
+			}
+
+			return nil, fmt.Errorf("http request returned non-200 response: %s. Message: %s",
+				resp.Status, failedResp.Message)
+		}
+
+		if respErr != nil {
+			return nil, fmt.Errorf("reading the response: %w", err)
+		}
+
+		bl := blocks.BlockResponseList{}
+		if err := json.Unmarshal(data, &bl); err != nil {
+			return nil, fmt.Errorf("could not unmarshal response: %w", err)
+		}
+
+		hasMore = bl.Response.HasMore
+		startCursor = bl.Response.NextCursor
+
+		ret = append(ret, bl.Results...)
+	}
+
+	return ret, nil
+}
